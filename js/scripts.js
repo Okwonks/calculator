e494aed99f8658217f96503bb05aed36abc0a9ab
David Kahara <kaharadave@gmail.com>
2017-05-30 12:20:09 +0300

    Initial commit for calculator app

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+var add = function(number1, number2){return number1 + number2};
+var number1 =parseInt( prompt("Enter Number1"))
+var number2 = parseInt(prompt("Enter another Number"))
+    alert(add(number1, number2));
